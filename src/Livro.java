fad1a90c416b02d9ccf9f28592be3e75b4db3608
FabioHSNavarro <fabioanhaia16@gmail.com>
2026-03-16 08:09:26 -0300

    Commit inicial: Projeto de LIvraria virtual

---
+++ src/Livro.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class Livro{
+    ///tipo nomeVariavel;
+    String titulo; String autor; String narrador; String editora; String versao; String resumo;
+    LocalTime duracao;
+    double preco;
+
