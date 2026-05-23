1c7088767660592636032356bbca8793c6bcced9
Fabio <fabioanhaia16@gmail.com>
2026-05-23 19:18:23 -0300

    Encapsulamento de atributos de Avaliacao e Livro

---
+++ src/br/com/etechoracio/playlivros/test/TestaUsuario.java
@@ -0,0 +1,9 @@
+package br.com.etechoracio.playlivros.test;
+
+import br.com.etechoracio.playlivros.model.Usuario;
+
+public class TestaUsuario {
+    public static void main(String[] args) {
+        Usuario cliente = new Usuario("Horacio");
+    }
+}
